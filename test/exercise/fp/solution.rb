781b3c70aeece0cdb98acb800900f58740a77fbf
nbadin <nbadin011@gmail.com>
2021-11-17 22:31:03 +0300

    lint code

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -6,16 +6,15 @@ module Exercise
       # film["genres"], film["year"], film["access_level"], film["country"]
       def rating(array)
         filtered_films = array.filter do |film|
-          film["rating_kinopoisk"].to_f.positive? && film["country"].to_s.split(',').size > 1
+          film['rating_kinopoisk'].to_f.positive? && film['country'].to_s.split(',').size > 1
         end
-        raiting_sum = filtered_films.reduce(0) { |acc, film| acc + film["rating_kinopoisk"].to_f }
+        raiting_sum = filtered_films.reduce(0) { |acc, film| acc + film['rating_kinopoisk'].to_f }
         raiting_sum / filtered_films.count
       end
 
       def chars_count(films, threshold)
-        filtered_films = films.filter { |film| film["rating_kinopoisk"].to_f >= threshold.to_f }
-        count_of_char = filtered_films.map{ |film| film["name"] }
-          .reduce(0) { |acc, name| acc + name.count('и') }
+        filtered_films = films.filter { |film| film['rating_kinopoisk'].to_f >= threshold.to_f }
+        filtered_films.map { |film| film['name'] }.reduce(0) { |acc, name| acc + name.count('и') }
       end
     end
   end
